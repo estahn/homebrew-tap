dcc90be5ce999da29b5c9fddf8c8fd8fa4287196
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-14 12:19:33 +1100

    Brew formula update for cloudping version v1.1.4

--- Formula/cloudping.rb
+++ Formula/cloudping.rb
@@ -1,9 +1,9 @@
 class Cloudping < Formula
   desc "cloudping identifies the cloud provider regions geographically closest and returns them in order of lowest to highest latency."
   homepage "https://github.com/estahn/cloudping"
-  url "https://github.com/estahn/cloudping/releases/download/v1.1.3/cloudping_1.1.3_darwin_amd64.zip"
-  version "1.1.3"
-  sha256 "485205f3dc3033d9964b77f50aa4c44630e8da1df70ea92045ff4ecd40716350"
+  url "https://github.com/estahn/cloudping/releases/download/v1.1.4/cloudping_1.1.4_darwin_amd64.zip"
+  version "1.1.4"
+  sha256 "66ca7fc2fd5b736ecb6e734f021814814a2a39b881bffbc99c67ec19796d84df"
 
   def install
     bin.install "cloudping"
